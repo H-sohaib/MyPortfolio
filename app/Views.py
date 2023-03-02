86abf562bf83c6ea08055102c1c7b36543830227
H_Sohaib <harraoui.sohaib1@gmail.com>
2023-03-02 17:05:11 +0100

    pull commit

--- app/Views.py
+++ app/Views.py
@@ -1,9 +1,10 @@
 # from app import demo_bp
 from app import app, mail
-from flask import render_template, send_from_directory, abort, request, Blueprint
+from flask import render_template, send_from_directory, abort, request, Blueprint, redirect
 from flask_mail import Message
 import subprocess
 from time import sleep
+import os
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -41,3 +42,44 @@ def download(file):
 @app.route("/works")
 def works():
     return render_template("work.html")
+
+
+demo_bp = Blueprint('demo_bp', __name__)
+
+
+@demo_bp.route('/TaskAPP')
+def taskapp():
+
+    if os.getcwd() == "E:\WorkDir\MyPortfolio":
+        print("directory chenged")
+        os.chdir("app/static/Projects/TaskAPPjQuery")
+
+    print(os.getcwd())
+    out = subprocess.run([".venv\\Scripts\\activate.bat"],
+                         stdout=subprocess.PIPE,
+                         text=True,
+                         )
+    # print("activate !!")
+    # print(out)
+    out = subprocess.run(["python", "query.py"],
+                         stdout=subprocess.PIPE,
+                         text=True,
+                         )
+    # print("create Tables !!")
+    # print(out)
+    # out = subprocess.run(["flask", "run", "-h", "localhost", "-p", "3000"],
+    #                      stdout=subprocess.PIPE,
+    #                      text=True,
+    #                      )
+    out = subprocess.Popen(["flask", "run", "-h", "0.0.0.0", "-p", "3000"],
+                           stdout=subprocess.PIPE,
+                           text=True
+                           )
+    print("python out : ")
+    print(out)
+    sleep(1)
+
+    return redirect("http://192.168.233.219:3000/")
+
+
+app.register_blueprint(demo_bp, url_prefix='/demo')
--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 class Config(object):
     TESTING = False
     SECRET_KEY = "jigqibcuqouqqbv"
-    DOWNLOAD_PATH = "E:/WorkDir/MyPortfolio/app/static/sendFiles"
+    DOWNLOAD_PATH = "F:/WorkDir/MyPortfolio/app/static/sendFiles"
     # DOWNLOAD_PATH = "/home/HarraouiSohaib/MyPortfolio/app/static/sendFiles"
     # mail config
     MAIL_SERVER = "smtp.gmail.com"
